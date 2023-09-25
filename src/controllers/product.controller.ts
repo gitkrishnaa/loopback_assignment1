6a97666bc278540f1e993ea6c573990eecb5f3de
krishna kumar <kri821084@gmail.com>
2023-09-25 22:07:58 +0530

    uploading on git hub

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,201 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {Product} from '../models';
+import {ProductRepository} from '../repositories';
+const CsvParser = require("json2csv").Parser;
+export class ProductController {
+  constructor(
+    @repository(ProductRepository)
+    public productRepository : ProductRepository,
+  ) {}
+
+  @post('/products')
+  @response(200, {
+    description: 'Product model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Product)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Product, {
+            title: 'NewProduct',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    product: Omit<Product, 'id'>,
+  ): Promise<Product> {
+    return this.productRepository.create(product);
+  }
+
+  @get('/products/count')
+  @response(200, {
+    description: 'Product model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count( @param.where(Product) where?: Where<Product>,): Promise<Count> {
+    return this.productRepository.count(where);
+  }
+
+
+  @get('/downloadProductsCSV')
+  @response(200, {
+    description: 'downoload from all data in csv format',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Product, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async csvDownload(){
+console.log("uguyg")
+const data_x=await this.productRepository.find();
+console.log(data_x,"data_x")
+
+const csvFields = ["id", "name", "price", "quantity"];
+    const csvParser = new CsvParser({ csvFields });
+    const csvData = csvParser.parse(data_x);
+    console.log(csvData,"dcsvDataata_x")
+// console.log(this.productRepository)
+    return csvData;
+  }
+
+
+
+  @get('/downloadProductsJSON')
+  @response(200, {
+    description: 'downoload from all data in json format',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Product, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async jsonDownload(){
+console.log("uguyg")
+const data_x=await this.productRepository.find();
+console.log(data_x,"data_x")
+
+
+// console.log(this.productRepository)
+    return data_x;
+  }
+
+
+  @get('/products')
+  @response(200, {
+    description: 'Array of Product model instances',
+    content: {
+      'text/csv': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Product, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Product) filter?: Filter<Product>,
+  ): Promise<Product[]> {
+console.log(filter,"uguyg")
+
+    return this.productRepository.find(filter);
+  }
+
+  @patch('/products')
+  @response(200, {
+    description: 'Product PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Product, {partial: true}),
+        },
+      },
+    })
+    product: Product,
+    @param.where(Product) where?: Where<Product>,
+  ): Promise<Count> {
+    return this.productRepository.updateAll(product, where);
+  }
+
+  @get('/products/{id}')
+  @response(200, {
+    description: 'Product model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Product, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(Product, {exclude: 'where'}) filter?: FilterExcludingWhere<Product>
+  ): Promise<Product> {
+    return this.productRepository.findById(id, filter);
+  }
+
+  @patch('/products/{id}')
+  @response(204, {
+    description: 'Product PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Product, {partial: true}),
+        },
+      },
+    })
+    product: Product,
+  ): Promise<void> {
+    await this.productRepository.updateById(id, product);
+  }
+
+  @put('/products/{id}')
+  @response(204, {
+    description: 'Product PUT success',
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() product: Product,
+  ): Promise<void> {
+    await this.productRepository.replaceById(id, product);
+  }
+
+  @del('/products/{id}')
+  @response(204, {
+    description: 'Product DELETE success',
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.productRepository.deleteById(id);
+  }
+}
